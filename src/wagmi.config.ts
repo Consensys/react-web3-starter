e57222fd71176470aef345395cf5b8cadc88608f
Alejandro <smarketing.cuz@gmail.com>
2024-03-20 09:35:19 -0500

    Wagmi working with the injected connector

--- src/wagmi.config.ts
+++ src/wagmi.config.ts
@@ -2,22 +2,9 @@ import { http, createConfig } from "wagmi";
 import { mainnet, sepolia, lineaTestnet } from "wagmi/chains";
 import { metaMask } from "wagmi/connectors";
 
-declare module "wagmi" {
-  interface Register {
-    config: typeof config;
-  }
-}
-
 export const config = createConfig({
-  chains: [mainnet, sepolia, lineaTestnet],
-  connectors: [
-    metaMask({
-      dappMetadata: {
-        name: "Example dapp",
-      },
-      infuraAPIKey: "YOUR_INF",
-    }),
-  ],
+  chains: [lineaTestnet],
+  connectors: [metaMask()],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
